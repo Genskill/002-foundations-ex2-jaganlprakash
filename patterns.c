32897ad69cb178cf714de511f05dbcbbc7f414a8
Jagan L Prakash <84394633+jaganlprakash2000@users.noreply.github.com>
2021-05-21 15:53:12 +0530

    Add files via upload

--- patterns.c
+++ patterns.c
@@ -1,4 +1,18 @@
-n--;
+#include<stdio.h>
+#include<cs50.h>
+
+
+int main(void)
+{
+    int p = get_int("Enter type of pattern (1 or 2) ");
+    int n = get_int("Number of rows");
+    if (p == 1) {
+        while (n != 0) {
+            for (int i = 0; i < n; i++) {
+                printf("#");
+            }
+            printf("\n");
+            n--;
         }
  }
 
