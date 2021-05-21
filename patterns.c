e5d5bd9164cd2b28978211ac2161358de8610aab
Jagan L Prakash <84394633+jaganlprakash2000@users.noreply.github.com>
2021-05-21 15:47:14 +0530

    Create patterns.c

---
+++ patterns.c
@@ -0,0 +1,16 @@
+n--;
+        }
+ }
+
+  else if(p==2){
+  for(int i=1; i<=n; i++){
+    for(int j=1; j<=n-i; j++){
+       printf(" ");
+     }
+    for(int k=1; k<=i;k++){
+     printf("#");
+    }
+   printf("\n");
+   }
+  }
+}
